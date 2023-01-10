ae7678cd0403032929e9424b59a104f5c0b52bf8
francoisdouillet <francois.douillet@sfr.fr>
2023-01-10 16:57:12 +0100

    image2

--- src/components/home/PhotoGroup.js
+++ src/components/home/PhotoGroup.js
@@ -1,17 +1,16 @@
 const PhotoGroup = ({ group, images, id }) => {
-
-
   return (
     <div className="photos__group" id={id}>
       <p>{group.toUpperCase()}</p>
       {images.map((image, i) => (
         <a
+          href={image}
           data-pswp-width="6048px"
           data-pswp-height="4008px"
           target="_blank"
           rel="noreferrer"
         >
-          <img key={i} alt="hugophoto" src={image}  />
+          <img key={i} alt="hugophoto" src={image} />
         </a>
       ))}
     </div>
